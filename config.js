9b4e13d12757016c730dda4f834209ac3b944364
Linus Miller <lohfu@lohfu.io>
2015-07-27 19:31:21 +0200

    Setting module paths no longer required for browserify.

--- config.js
+++ config.js
@@ -61,7 +61,7 @@ var browserify = {
 		entries: [ 
 			'app.js'
 		],
-		paths: [ path.join(PWD, 'node_modules/hats'), path.join(PWD, 'hats') ],
+		//paths: [ path.join(PWD, 'node_modules/hats'), path.join(PWD, 'hats') ],
 		// outputs only need to be used if output names are different from entries. Otherwise the entries array is copied into the outputs array.
 		outputs: [
 			'app.js'
