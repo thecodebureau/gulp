6f739dad4590811ffe6f515410aa6c317c7d91e8
Linus Miller <lohfu@lohfu.io>
2015-10-30 20:23:34 +0100

    Created executable.

---
+++ bin/tcb-gulp.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+var p = require('path'),
+	fs = require('fs'),
+	spawn = require('child_process').spawn;
+
+var tcbGulpPath = p.dirname(__dirname),
+	gulpExecutable = p.join(tcbGulpPath, 'node_modules/.bin/gulp');
+
+if(!fs.existsSync(gulpExecutable)) {
+	gulpExecutable = p.join(p.dirname(tcbGulpPath), '.bin/gulp');
+}
+
+var gulpProcess = spawn(gulpExecutable, [ '--cwd', tcbGulpPath ], { stdio: 'inherit' });
+
+gulpProcess.on('close', function() {
+	console.log('Gulp process exited');
+	run();
+});
