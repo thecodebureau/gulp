768accbe07e9b8c741c0b92a71f8e234db93e8f8
Linus Miller <lohfu@lohfu.io>
2015-10-31 21:23:30 +0100

    Much smarter nodemon. Listens to stdout to know when app has finished bootup and is listening.

--- tasks/development/nodemon.js
+++ tasks/development/nodemon.js
@@ -1,21 +1,27 @@
-var gulp        = require('gulp');
 var browserSync = require('browser-sync');
 var nodemon = require('gulp-nodemon');
 
-/**
- *  * Run the build task and start a server with BrowserSync
- *   */
 module.exports = function(gulp, config) {
 	config = config.nodemon || config;
+
 	gulp.task('nodemon', function(cb) {
-		nodemon(config)
-		.on('start', function () {
-			// 'start' event gets called BEFORE running the script, or so it seems.
-			// Wait a little while to reload browserSync to let server reboot
-			if(browserSync.active)
-				setTimeout(browserSync.reload, config.reloadDelay || 500);
-			else
-				cb();
-		});
+		nodemon(_.defaults({ stdout: false }, config))
+			.on('readable', function(data) {
+				this.stdout.pipe(process.stdout);
+				this.stderr.pipe(process.stderr);
+
+				this.stdout.on('data', function(chunk) {
+					if (/Express server started on port/.test(chunk)) {
+						if(cb)
+							cb();
+
+						cb = null;
+
+						if(browserSync.active)
+							browserSync.reload();
+					}
+				});
+
+			});
 	});
 };
