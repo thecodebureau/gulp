53c36c3bcb14cf080a685bf4aba24bb97e4fcc40
Linus Miller <lohfu@lohfu.io>
2015-10-31 18:56:38 +0100

    Supress symlink plugins default logs, and output total symlink count at end for svg, raster and font tasks.

--- tasks/fonts.js
+++ tasks/fonts.js
@@ -1,11 +1,30 @@
-var path = require('path');
+// modules > native
+var p = require('path');
+
+// modules > 3rd party
+var chalk = require('chalk');
+
+// modules > gulp:utilities
+var gutil = require('gulp-util');
+
+// modules > gulp:plugins
 var symlink = require('gulp-symlink');
+
+var TASK_NAME = 'fonts';
 
 module.exports = function(gulp, config) {
 	config = config.fonts || config;
-	gulp.task('fonts', function() {
-		gulp.src(config.src).pipe(symlink(function(file) {
-			return path.join(config.dest, file.relative);
-		}));
+
+	gulp.task(TASK_NAME, function() {
+		var count = 0;
+
+		gulp.src(config.src)
+			.on('end', function() {
+				gutil.log(chalk.cyan(TASK_NAME) + ' done symlinking ' + chalk.bold.blue(count) + ' files');
+			})
+			.pipe(symlink(function(file) {
+				count++;
+				return path.join(config.dest, file.relative);
+			}));
 	});
 };
--- tasks/svg.js
+++ tasks/svg.js
@@ -1,11 +1,30 @@
-var path = require('path');
+// modules > native
+var p = require('path');
+
+// modules > 3rd party
+var chalk = require('chalk');
+
+// modules > gulp:utilities
+var gutil = require('gulp-util');
+
+// modules > gulp:plugins
 var symlink = require('gulp-symlink');
+
+var TASK_NAME = 'svg';
 
 module.exports = function(gulp, config) {
 	config = config.svg || config;
-	gulp.task('svg', function() {
-			return gulp.src(config.src).pipe(symlink(function(file) {
-				return path.join(config.dest, file.relative);
-			}));
+
+	gulp.task(TASK_NAME, function() {
+		var count = 0;
+
+		return gulp.src(config.src)
+			.on('end', function() {
+				gutil.log(chalk.cyan(TASK_NAME) + ' done symlinking ' + chalk.bold.blue(count) + ' files');
+			})
+			.pipe(symlink(function(file) {
+				count++;
+				return p.join(config.dest, file.relative);
+			}, { log: false }));
 	});
 };
